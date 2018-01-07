3e0790b558c81c9f3f8444877df463e5d5bdb2d0
limeng@aixuedai.com <limeng@aixuedai.com>
2018-01-07 15:28:45 +0800

    添加新闻列表

--- src/routes/news.js
+++ src/routes/news.js
@@ -16,4 +16,26 @@ router.get('/news/:id', async(ctx, next) => {
     });
 })
 
+router.get('/news', async(ctx, next) => {
+    let result = await optionService.getNews()
+    let homeModel = await optionService.getHomeData()
+    let solutionModel = []
+    if (result.thumbnails && result.solutionArray) {
+        for (let i = 0; i < result.thumbnails.length; i++) {
+            solutionModel.push({
+                thumbnail: result.thumbnails[i],
+                title: result.solutionArray[i].title,
+                abstract: result.solutionArray[i].abstract,
+                id: result.solutionArray[i]._id
+            })
+        }
+    }
+
+    await ctx.render('newslist', {
+        title: '新闻列表',
+        solutionModel,
+        homeModel
+    })
+})
+
 module.exports = router
