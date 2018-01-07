79e4694d1d2f0b9d384a8bd3b07da3579646f9c1
limeng@aixuedai.com <limeng@aixuedai.com>
2018-01-07 15:34:07 +0800

    添加新闻列表

--- src/routes/news.js
+++ src/routes/news.js
@@ -19,21 +19,23 @@ router.get('/news/:id', async(ctx, next) => {
 router.get('/news', async(ctx, next) => {
     let result = await optionService.getNews()
     let homeModel = await optionService.getHomeData()
-    let solutionModel = []
-    if (result.thumbnails && result.solutionArray) {
-        for (let i = 0; i < result.thumbnails.length; i++) {
-            solutionModel.push({
-                thumbnail: result.thumbnails[i],
-                title: result.solutionArray[i].title,
-                abstract: result.solutionArray[i].abstract,
-                id: result.solutionArray[i]._id
+
+    let goodNewsArray = []
+    let goodNewsObj = await optionService.getNews()
+    if (goodNewsObj.newsArray && goodNewsObj.thumbnails) {
+        for (let i = 0; i < goodNewsObj.newsArray.length; i++) {
+            goodNewsArray.push({
+                thumbnail: goodNewsObj.thumbnails[i],
+                title: goodNewsObj.newsArray[i].title,
+                abstract: goodNewsObj.newsArray[i].abstract,
+                id: goodNewsObj.newsArray[i]._id
             })
         }
     }
 
     await ctx.render('newslist', {
         title: '新闻列表',
-        solutionModel,
+        goodNewsArray,
         homeModel
     })
 })
